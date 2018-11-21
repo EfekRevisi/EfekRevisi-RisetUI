e391ae3736724d4be27485e9de0ffc75b632e576
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-11-21 21:47:40 +0700

    feat(navigation): layouting header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <ui-organism-header />
+    <ui-organism-header
+      photo="https://www.fakepersongenerator.com/Face/male/male1084415159882.jpg"
+    />
     <router-view />
   </div>
 </template>
--- src/components/general/organism/header/header.component.vue
+++ src/components/general/organism/header/header.component.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="ef-header relative flex z-index-100 mb-25">
-    <ui-atomic-icon type="material-icons">search</ui-atomic-icon>
+  <div class="ef-header relative z-index-100 mb-50">
+    <div class="ef-header__content flex">
+      <div class="ef-header__title">
+        <h1>Cari Restoran</h1>
+        <p>Jangan lupa makan hari ini, fareel</p>
+      </div>
+      <img
+        :src="photo"
+        class="ef-header__avatar"
+      >
+    </div>
+    <ui-atomic-textfield
+      :border="false"
+      name="search"
+      icon="near_me"
+      shadow
+      reverse
+      placeholder="Enter keywords.."
+    />
   </div>
 </template>
 
@@ -8,3 +25,10 @@
 @import '@/style/scss/import.scss';
 @import './style.scss';
 </style>
+
+<script>
+import props from './prop-types'
+export default {
+  props
+}
+</script>
